8ceb95a44c9c2e687cb399956ebb6ab667aca7cd
perinze <perinzefaper@foxmail.com>
2023-06-25 00:28:59 +0800

    add formatter to types

--- lib/types.ml
+++ lib/types.ml
@@ -8,3 +8,14 @@ type ty =
 | ARRAY of ty * unique
 | NAME of Symbol.symbol * ty option ref (* place holder for recursion *)
 | UNIT
+
+let rec format = function
+| RECORD (sym_ty_list, _) ->
+  let fmt (sym, ty) = (Symbol.name sym) ^ " : " ^ (format ty) in
+  "{" ^ (List.fold_left (fun acc a -> acc ^ "; " ^ (fmt a)) "" sym_ty_list) ^ "}"
+| NIL -> "nil"
+| INT -> "int"
+| STRING -> "string"
+| ARRAY (ty, _) -> "[" ^ (format ty) ^ "]"
+| NAME _ -> "name"
+| UNIT -> "unit"
