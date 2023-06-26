216d5de4b170fac424f751f827d306b2c2d3beeb
perinze <perinzefaper@foxmail.com>
2023-06-27 00:25:18 +0800

    make it read file or stdin

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,12 @@
 open Tiger
 
 let () =
-  Parzer.parse Lexer.token (Lexing.from_channel stdin)
+  let args = Sys.argv in
+  let argc = Array.length args in
+  let channel =
+    match argc with
+    | 1 -> stdin
+    | _ -> open_in args.(1)
+  in
+  Parzer.parse Lexer.token (Lexing.from_channel channel)
   |> Semant.trans_prog
