e26e772d2df386c31ea3198a0e445cdeb60eafc2
perinze <perinzefaper@foxmail.com>
2023-06-28 22:54:43 +0800

    update util functions

--- lib/util.ml
+++ lib/util.ml
@@ -1,20 +1,34 @@
 let mkbuf s =
   Lexing.from_string s
 
-let tokens s =
+let lex lexbuf =
   let lst = ref [] in
-    let lexbuf = mkbuf s in
-      let loop = ref true in
-        while !loop do
-          let result = Lexer.token lexbuf in
-            lst := !lst @ [result];
-            if result = Parzer.EOF then
-              loop := false
-        done;
-      !lst
+    let loop = ref true in
+    while !loop do
+      let result = Lexer.token lexbuf in
+      lst := !lst @ [result];
+      if result = Parzer.EOF then
+        loop := false
+    done;
+    !lst
 
-let parze s =
-  Parzer.parse Lexer.token (mkbuf s)
+let lex_string s =
+  lex (mkbuf s)
 
-let type_check s =
-  parze s |> Semant.trans_prog
+let lex_channel c =
+  lex (Lexing.from_channel c)
+
+let parze lexbuf =
+  Parzer.parse Lexer.token lexbuf
+
+let parze_string s =
+  parze (mkbuf s)
+
+let parze_channel c =
+  parze (Lexing.from_channel c)
+
+let type_check_string s =
+  parze_string s |> Semant.trans_prog
+
+let type_check_channel c =
+  parze_channel c |> Semant.trans_prog
