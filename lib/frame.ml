1db231384b4cc193812252fa9c467f66994c42ce
perinze <perinzefaper@foxmail.com>
2023-08-10 03:17:53 +0800

    modify frame

---
+++ lib/frame.ml
@@ -0,0 +1,7 @@
+type frame = Rvframe.frame
+type access = Rvframe.access
+
+let new_frame = Rvframe.new_frame
+let name = Rvframe.name
+let formals = Rvframe.formals
+let alloc_local = Rvframe.alloc_local
--- lib/frame.mli
+++ lib/frame.mli
@@ -1,7 +1,7 @@
 type frame
 type access
 
-val new_frame : (name : Temp.label) -> (formals : bool list) -> frame
+val new_frame : Temp.label -> bool list -> frame
 val name : frame -> Temp.label
 val formals : frame -> access list
 val alloc_local : frame -> bool -> access
